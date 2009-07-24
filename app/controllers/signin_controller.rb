5daba701b98b51becff1cac85f7559ac22a313e3
Jonatan Kallus <jonatan.kallus@gmail.com>
2009-07-24 20:23:40 +0100

    generated signin controller and views etc

---
+++ app/controllers/signin_controller.rb
@@ -0,0 +1,5 @@
+class SigninController < ApplicationController
+  def index
+  end
+
+end
---
+++ test/functional/signin_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class SigninControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
