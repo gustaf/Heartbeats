aa523b3d98879b79dc1d4cb3eb4e54aacf9c3f51
Jonatan Kallus <jonatan.kallus@gmail.com>
2009-07-24 20:27:15 +0100

    set up signin controller as root

--- config/routes.rb
+++ config/routes.rb
@@ -38,4 +38,5 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  map.root :controller => 'signin'
 end
