6c1266bf6720cf82c092f83d10b2a7574e21c097
Gustaf Alstromer <gustaf@gmail.com>
2009-11-25 15:30:36 -0800

    ...

--- app/models/user_publisher.rb
+++ app/models/user_publisher.rb
@@ -1,7 +1,7 @@
 class UserPublisher < Facebooker::Rails::Publisher
   def playlist_added_template
-    one_line_story_template "{*actor*} shared a Spotify playlist on SpotifyConnect."
-    stories = ["{*actor*} shared a Spotify playlist on SpotifyConnect.","Share your playlist on SpotifyConnect <a href='http://localhost:9930/'>here</a>! {*target*} shared their playlists on <a href='http://localhost:9930/'>SpotifyConnect</a>."]
+    one_line_story_template "{*actor*} shared a Spotify playlist on Heartbeats."
+    stories = ["{*actor*} shared a Spotify playlist on Heartbeats.","Share your playlist on Heartbeats <a href='http://heartb.com'>here</a>! {*target*} shared their playlists on <a href='http://heartb.com'>Heartbeats</a>."]
     short_story_template *stories
     full_story_template *stories
   end
