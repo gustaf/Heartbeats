eece7253b6b81847a2446adb89d124992045d1ba
Gustaf Alstromer <gustaf@gmail.com>
2009-11-25 21:30:33 -0800

    ...

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-#PAGE TITLES
-  def title(page_title)
-    content_for(:title) { page_title }
 end
