08a9aeb7805928bda013701de2088b464e033d7e
Joseph Sofaer <joseph.sofaer@gmail.com>
2009-11-29 22:27:35 -0800

    split friends into two columns

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,16 @@ class HomeController < ApplicationController
     @my_playlists = Playlist.find_all_by_user_id(@user.id)
     
     friends = facebook_session.user.friends
-    @friends_in_app = User.find(:all, :conditions => ["uid IN (?)", friends.map{|f| f.uid}], :include => :playlists, :limit => 16)
+    friends_in_app = User.find(:all, :conditions => ["uid IN (?)", friends.map{|f| f.uid}], :include => :playlists)
+    @friends_in_left_col = []
+    @friends_in_right_col = []
+    i = 0
+    friends_in_app.each do |friend|
+      if (i = i ^ 1) == 1
+        @friends_in_left_col << friend
+      else
+        @friends_in_right_col << friend
+      end
+    end
   end
 end
