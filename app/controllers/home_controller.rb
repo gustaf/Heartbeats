e658117346207d452c21ec0f7b13a1cbcef4e839
Joseph Sofaer <joseph.sofaer@gmail.com>
2009-11-29 21:48:58 -0800

    more styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,6 +8,6 @@ class HomeController < ApplicationController
     @my_playlists = Playlist.find_all_by_user_id(@user.id)
     
     friends = facebook_session.user.friends
-    @friends_in_app = User.find(:all, :conditions => ["uid IN (?)", friends.map{|f| f.uid}], :include => :playlists)
+    @friends_in_app = User.find(:all, :conditions => ["uid IN (?)", friends.map{|f| f.uid}], :include => :playlists, :limit => 16)
   end
 end
