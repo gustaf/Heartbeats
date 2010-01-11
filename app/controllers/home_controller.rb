25b9742526c2f520219a86523fa3dbe79f1e74cd
Jonatan Kallus <jonatan@jonatan-kalluss-macbook.local>
2010-01-12 00:37:48 +0100

    changes on home page: displaying self as friend, max 3 lists per user, more link, name links to user page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,8 @@ class HomeController < ApplicationController
   helper_method :facebook_session
 
   def index
-    @new_playlist = Playlist.new
-    
-    @my_playlists = Playlist.find_all_by_user_id(@user.id)
-    
     friends = facebook_session.user.friends
-    friends_in_app = User.find(:all, :conditions => ["uid IN (?)", friends.map{|f| f.uid}], :include => :playlists)
+    friends_in_app = [@user] + User.find(:all, :conditions => ["uid IN (?)", friends.map{|f| f.uid}], :include => :playlists)
     @friends_in_left_col = []
     @friends_in_right_col = []
     i = 0
