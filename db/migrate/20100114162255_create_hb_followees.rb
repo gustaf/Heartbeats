93091f2ff9c9c2af2d3e87af84f28d45adfcfb01
Jonatan Kallus <jonatan@jonatan-kalluss-macbook.local>
2010-01-14 20:33:28 +0100

    uid fields werent long enough

--- db/migrate/20100114162255_create_hb_followees.rb
+++ db/migrate/20100114162255_create_hb_followees.rb
@@ -1,7 +1,7 @@
 class CreateHbFollowees < ActiveRecord::Migration
   def self.up
     create_table :hb_followees do |t|
-      t.integer :uid, :limit => 15
+      t.integer :uid, :limit => 20
       t.references :user
 
       t.timestamps
