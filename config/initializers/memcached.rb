4690c3f4a0a2237e7c357bbd760b8f251f507e95
Gustaf Alstromer <gustaf@gmail.com>
2010-01-18 15:44:57 +0100

    updated css on playlist page

--- config/initializers/memcached.rb
+++
@@ -1,6 +0,0 @@
-if ENV['MEMCACHE_SERVERS']
-  require 'memcache'
-  servers = ENV['MEMCACHE_SERVERS'].split(',')
-  namespace = ENV['MEMCACHE_NAMESPACE']
-  CACHE = MemCache.new(servers, :namespace => namespace)
-end
