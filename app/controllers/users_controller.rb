77f553085e67874d700791e0e65bc93f8fff8c5a
Gustaf Alstromer <gustaf@gmail.com>
2010-01-19 22:30:42 +0100

    new css on header and footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :set_facebook_session, :ensure_logged_in_and_get_user
+  helper_method :facebook_session
   
   def index
     render :text => "users/index"
