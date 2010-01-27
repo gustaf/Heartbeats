e7fb6e5cd574b7e537f32f47cf6640c5487f1667
Gustaf Alstromer <gustaf@gmail.com>
2010-01-27 04:41:47 +0100

    fix 3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 require 'bandsintown'
-
+ 
 class HomeController < ApplicationController
   before_filter :set_facebook_session, :ensure_logged_in_and_get_user
   helper_method :facebook_session
-
+ 
 def index
     @friends = [@user] + @user.followees
     Bandsintown.app_id = "heartbeats"
