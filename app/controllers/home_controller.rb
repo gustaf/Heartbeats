32eab43958ece9d7fde47493dd41443f1318b6c1
Joseph Sofaer <joseph.sofaer@gmail.com>
2010-01-26 23:46:51 -0800

    add methods back into home controller...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,27 +4,27 @@ class HomeController < ApplicationController
   before_filter :set_facebook_session, :ensure_logged_in_and_get_user
   helper_method :facebook_session
  
-#def index
-#    @friends = [@user] + @user.followees
-#    Bandsintown.app_id = "heartbeats"
-#    begin
-#      @events = Bandsintown::Event.recommended({
-#        :artists => @user.top50artists,
-#        :location => request.remote_ip
-        #:location => "212.162.1.95"
-#      })
-#    rescue Bandsintown::APIError
-#      @events = []
-#    end
-#  end
-#  def aboutus
-#  end
-#  def help
-#  end
-#  def contact
-#  end
-#  def terms
-#  end
-#  def privacy
-#  end
+def index
+    @friends = [@user] + @user.followees
+    Bandsintown.app_id = "heartbeats"
+    begin
+      @events = Bandsintown::Event.recommended({
+        :artists => @user.top50artists,
+        :location => request.remote_ip
+       #:location => "212.162.1.95"
+      })
+    rescue Bandsintown::APIError
+      @events = []
+    end
+  end
+  def aboutus
+  end
+  def help
+  end
+  def contact
+  end
+  def terms
+  end
+  def privacy
+  end
 end
