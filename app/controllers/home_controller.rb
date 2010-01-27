ee4617f162b1f8a67fa863cff0fb1db533302f67
Gustaf Alstromer <gustaf@gmail.com>
2010-01-27 05:48:24 +0100

    changed homecontroller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,30 +1,30 @@
-#require 'bandsintown'
+require 'bandsintown'
  
 class HomeController < ApplicationController
   before_filter :set_facebook_session, :ensure_logged_in_and_get_user
   helper_method :facebook_session
  
-#def index
-#    @friends = [@user] + @user.followees
-#    Bandsintown.app_id = "heartbeats"
-#    begin
-#      @events = Bandsintown::Event.recommended({
-#        :artists => @user.top50artists,
-#        :location => request.remote_ip
+def index
+    @friends = [@user] + @user.followees
+    Bandsintown.app_id = "heartbeats"
+    begin
+      @events = Bandsintown::Event.recommended({
+        :artists => @user.top50artists,
+        :location => request.remote_ip
         #:location => "212.162.1.95"
-#      })
-#    rescue Bandsintown::APIError
-#      @events = []
-#    end
-#  end
-#  def aboutus
-#  end
-#  def help
-#  end
-#  def contact
-#  end
-#  def terms
-#  end
-#  def privacy
-#  end
+      })
+    rescue Bandsintown::APIError
+      @events = []
+    end
+  end
+  def aboutus
+  end
+  def help
+  end
+  def contact
+  end
+  def terms
+  end
+  def privacy
+  end
 end
