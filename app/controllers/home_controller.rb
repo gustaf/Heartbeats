f9daa3b882a05bfc664c723b5ee0079a42ca144c
Gustaf Alstromer <gustaf@gmail.com>
2010-01-29 16:35:41 +0100

    fixing bandsintown

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,16 +11,7 @@ def index
       @events = Bandsintown::Event.recommended({
         :artists => @user.top50artists,
         :location => request.remote_ip
-
-        # London
-        #:location => "212.162.1.95"
-
-        #San Francisco
-        #:location => "198.144.201.210"
-        
-        #Stockholm
-        #:location => "194.14.56.70"
-        
+       #:location => "212.162.1.95"
       })
     rescue Bandsintown::APIError
       @events = []
