12bd50a26affac10784443068f9387f4711ba882
Jonatan Kallus <jonatan@jonatan-kalluss-macbook.local>
2010-04-03 23:14:35 +0200

    added controller for playlist app to get missing playlists

---
+++ app/controllers/missing_playlists_controller.rb
@@ -0,0 +1,10 @@
+class MissingPlaylistsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def index
+    playlists = Playlist.all(:conditions => ["title IS NULL"])
+    urls = playlists.select{|p| p.is_proper_playlist?}.map{|p| p.url_spotify}
+    
+    render :text => urls.join("\n")
+  end
+end
