f2c363efb5561447da13e840cc997d347cc9c15f
Jonatan Kallus <jonatan@jonatan-kalluss-macbook.local>
2010-04-05 01:22:44 +0200

    jquery ajax for more on home

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,16 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function (){
+    $("div.more/a").click(function() {
+      $("div.hidden_playlist", $(this).parent()).slideToggle();
+      if($("a", this).html() == 'less') {
+        $("a", this).html('more');
+        $("span", $(this)).html('&raquo;');
+      } else {
+        $("a", this).html('less');
+        $("span", $(this)).html('&laquo;');
+      }
+      //$("a", this).toggle(function() {$(this).html('less');}, function() {$(this).html('more');});
+      });
+    });
