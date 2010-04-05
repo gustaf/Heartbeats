f7c9a339da78156c939617a9654c0ad319f49f3b
Jonatan Kallus <jonatan@jonatan-kalluss-macbook.local>
2010-04-05 13:39:09 +0200

    like/unlike with ajax. heartbeats first real ajax!

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,15 +3,22 @@ class LikesController < ApplicationController
   helper_method :facebook_session
   
   def create
-    playlist = Playlist.find(params["playlist_id"])
-    @user.like(playlist)
-    redirect_to playlist_url(playlist)
+    @playlist = Playlist.find(params["playlist_id"])
+    @user.like(@playlist)
+    respond_to do |format|
+      format.html {redirect_to playlist_url(@playlist)}
+      format.js
+    end
   end
   
   def destroy
     like = Like.find(params[:id])
     like.destroy if like.user_id == @user.id
-    redirect_to playlist_url(like.playlist_id)
+    @playlist = Playlist.find like.playlist_id
+    respond_to do |format|
+      format.html {redirect_to playlist_url(like.playlist_id)}
+      format.js
+    end
   end
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,26 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+jQuery.ajaxSetup({
+    "beforeSend": function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+    });
 
 $(document).ready(function (){
+    $("a.like_link").live("click", function() {
+      $.post($(this).attr("href"), null, null, "script");
+      return false;
+      });
+
+    $("a.unlike_link").live("click", function() {
+      $.ajax({
+        type:"delete",
+        url:$(this).attr("href"),
+        data:null,
+        success:null,
+        dataType:"script"
+        });
+      return false;
+      });
+
     $("div.more/a").click(function() {
       $("div.hidden_playlist", $(this).parent()).slideToggle();
       if($("a", this).html() == 'less') {
@@ -11,6 +30,9 @@ $(document).ready(function (){
         $("a", this).html('less');
         $("span", $(this)).html('&laquo;');
       }
-      //$("a", this).toggle(function() {$(this).html('less');}, function() {$(this).html('more');});
       });
     });
+
+function register_likes() {
+
+}
